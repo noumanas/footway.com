ee2e47f53149739a8b4f571ee047923ef18962d8
Nouman Qureshi <noumanqureshi15@gmail.com>
2022-03-14 10:53:37 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-// window.onscroll = function() {myFunction()};
+window.onscroll = function() {myFunction()};
+//Sticky Bar 
+var navbar = document.getElementById("header");
+var sticky = navbar.offsetTop;
 
-// var navbar = document.getElementById("header");
-// var sticky = navbar.offsetTop;
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
 
-// function myFunction() {
-//   if (window.pageYOffset >= sticky) {
-//     navbar.classList.add("sticky")
-//   } else {
-//     navbar.classList.remove("sticky");
-//   }
-// }
+//slider...
 
 var slideIndex = 1;
 showSlides(slideIndex);
@@ -37,3 +39,20 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+
+// search bar
+var search_bar = document.getElementById('search_box');
+var s_icon = document.getElementById('s-icons');
+
+search_bar.addEventListener('click',
+function search_action(){
+  search_bar.setAttribute('style',' height:300px; margin-top:260px; z-index:1; width:40%;');
+  // s_icon.setAttribute('style','background-color: white; height: 22px; color: black; padding: 10px; border-radius: 10px 0px 1px 10px;')
+}
+);
+search_bar.addEventListener('mouseleave',
+function search_action(){
+  search_bar.setAttribute('style',' height:50%; margin-top:0px; z-index:1; ');
+  // s_icon.setAttribute('style','background-color: ; height: 22px; color: black; padding: 10px; border-radius: 0;')
+}
+);
