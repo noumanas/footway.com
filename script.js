97b05b14a7468de7797210ec89651eaa65cb99c0
Nouman Qureshi <noumanqureshi15@gmail.com>
2022-03-14 18:21:12 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -43,16 +43,84 @@ function showSlides(n) {
 // search bar
 var search_bar = document.getElementById('search_box');
 var s_icon = document.getElementById('s-icons');
-
+var search_active =document.querySelector('.search-active');
 search_bar.addEventListener('click',
 function search_action(){
   search_bar.setAttribute('style',' height:300px; margin-top:260px; z-index:1; width:40%;');
+  search_active.style.display="block";
   // s_icon.setAttribute('style','background-color: white; height: 22px; color: black; padding: 10px; border-radius: 10px 0px 1px 10px;')
 }
 );
 search_bar.addEventListener('mouseleave',
 function search_action(){
   search_bar.setAttribute('style',' height:50%; margin-top:0px; z-index:1; ');
+  search_active.style.display="none";
   // s_icon.setAttribute('style','background-color: ; height: 22px; color: black; padding: 10px; border-radius: 0;')
 }
 );
+
+//left right slide
+let control = document.querySelector('#control');
+
+        control.addEventListener('click', function (e) {
+            // get the scrollDemo
+            let div = document.getElementById('brandShoes');
+            // get the target
+            let target = e.target;
+            // handle each button's click
+            switch (target.id) {
+                case 'btnScrollTop':
+                    div.scrollLeft += 100;
+                    break;
+
+                case 'btnScrollLeft':
+                    div.scrollLeft -= 100;
+                    break;
+            }
+        });
+
+//left right slide
+let control2 = document.querySelector('#control2');
+
+        control2.addEventListener('click', function (e) {
+            // get the scrollDemo
+            let div = document.getElementById('recommend_product');
+            // get the target
+            let target = e.target;
+            // handle each button's click
+            switch (target.id) {
+                case 'btnScrollright2':
+                    div.scrollLeft += 100;
+                    break;
+
+                case 'btnScrollLeft2':
+                    div.scrollLeft -= 100;
+                    break;
+            }
+        });
+//drop down
+var drop = document.querySelector('.dropup');
+var dropup = document.querySelector('.dropup-content');
+drop.addEventListener('click', function (e){
+  dropup.style.display="block";
+})
+dropup.addEventListener('mouseleave',function (e){
+  dropup.style.display="none";
+})
+
+
+//accordians
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
